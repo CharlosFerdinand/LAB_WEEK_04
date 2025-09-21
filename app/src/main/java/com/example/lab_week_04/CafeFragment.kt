07109ed25ac6536970f1303bdf50709525eb97ba
CharlosFerdinand <74392017+CharlosFerdinand@users.noreply.github.com>
2025-09-21 10:52:21 +0700

    Commit No. 4 – Assignment

--- app/src/main/java/com/example/lab_week_04/CafeFragment.kt
+++ app/src/main/java/com/example/lab_week_04/CafeFragment.kt
@@ -13,21 +13,38 @@ class CafeFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_cafe, container, false)
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val viewPager = view.findViewById<ViewPager2>(R.id.view_pager)
         val tabLayout = view.findViewById<TabLayout>(R.id.tab_layout)
-        val adapter = CafeAdapter(childFragmentManager, lifecycle)
+
+        // Titles for tabs
+        val titles = listOf(
+            getString(R.string.starbucks_title),
+            getString(R.string.janjijiwa_title),
+            getString(R.string.kopikenangan_title)
+        )
+
+        // Descriptions for content
+        val descriptions = listOf(
+            getString(R.string.starbucks_desc),
+            getString(R.string.janjijiwa_desc),
+            getString(R.string.kopikenangan_desc)
+        )
+
+        val adapter = CafeAdapter(childFragmentManager, lifecycle, descriptions)
         viewPager.adapter = adapter
+
         TabLayoutMediator(tabLayout, viewPager) { tab, position ->
-            tab.text = resources.getString(TABS_FIXED[position])
+            tab.text = titles[position]
         }.attach()
     }
 }
